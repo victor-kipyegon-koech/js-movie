fccf10f66d98a8714f4fa1c9281d4244b1b0237b
victor-kipyegon-koech <victorkipyegon778@gmail.com>
2025-10-25 07:38:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let time=12;
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+  
+}
+console.log(time)
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
